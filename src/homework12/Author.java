138efa735ca8408d08114fc51484f5132ea9b1f1
Лилиана <lilya_gazina@mail.ru>
2023-02-06 19:40:15 +0300

    homework is ready

--- src/homework12/Author.java
+++ src/homework12/Author.java
@@ -1,24 +1,20 @@
 package homework12;
 
-public class Author {
-    private String firstName;
-    private String space;
-    private String lastName;
-    public String author;
+import java.util.Objects;
 
-    public Author(String firstName, String space, String lastName) {
+public class Author {
+    private final String firstName;
+    private final String lastName;
+    public final String author;
+
+    public Author(String firstName,String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.space = space;
-        this.author = firstName + space + lastName;
+        this.author = firstName + lastName;
     }
 
     public String getFirstName() {
         return this.firstName;
-    }
-
-    public String getSpace() {
-        return this.space;
     }
 
     public String getLastName() {
@@ -29,4 +25,21 @@ public class Author {
         return this.author;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author1 = (Author) o;
+        return Objects.equals(firstName, author1.firstName) && Objects.equals(lastName, author1.lastName) && Objects.equals(author, author1.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, author);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + lastName;
+    }
 }
