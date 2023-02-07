7861eb2392f57ab92fb104153bac7433f3c6854c
Лилиана <lilya_gazina@mail.ru>
2023-02-07 16:13:58 +0300

    homework is ready

--- src/homework12/Author.java
+++ src/homework12/Author.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class Author {
     private final String firstName;
     private final String lastName;
-    public final String author;
+    private final String author;
 
     public Author(String firstName,String lastName) {
         this.firstName = firstName;
